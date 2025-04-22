d8b77c11690f55f2d406e358230e0ce07da897af
Sudipta <sudipta.anupam.datta@dohatec.com.bd>
2025-04-22 13:34:41 +0600

    binary search

--- binary_search.cpp
+++ binary_search.cpp
@@ -8,16 +8,17 @@ void takeArrayInput(int arr[], int size){
 }
 
 int binarySearch(int arr[], int left, int right, int key){
-    int mid = (left+right)/2;
+    if(left > right) return -1;
+    
+    int mid = left + (right - left)/2; //do this instead of (l+r)/2 so that mid can never go out of INT_MAX
+    
     if(arr[mid] == key) return mid;
+    
     else if(arr[mid] > key) {
         return binarySearch(arr, left, mid-1, key);
     }
-    else if(arr[mid] < key){
-        return binarySearch(arr, mid+1, right, key);
-    }
     else{
-        return -1;
+        return binarySearch(arr, mid+1, right, key);
     }
 }
 
@@ -29,6 +30,7 @@ int main() {
     cin >> key;
     
     int index = binarySearch(arr, 0, size-1, key);
+    
     
     cout << "Index of the key is : " << index << endl;
     
